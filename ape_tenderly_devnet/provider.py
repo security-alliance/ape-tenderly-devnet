6897b20dd85273ae1b82761e43835ebf329a5e32
Isaac Patka <ipatka@gmail.com>
2023-11-27 20:29:33 -0500

    log

--- ape_tenderly_devnet/provider.py
+++ ape_tenderly_devnet/provider.py
@@ -237,12 +237,14 @@ class TenderlyDevnetProvider(Web3Provider, TestProviderAPI):
                 txn_dict["type"] = HexBytes(txn_dict["type"]).hex()
 
             tx_params = cast(TxParams, txn_dict)
+            print(f"Tx params: {tx_params}")
             if (self._default_gas is not None):
                 # tx_params["maxFeePerGas"] = self._default_gas
                 tx_params["gasPrice"] = self._default_gas
                 tx_params["type"] = 0
                 # tx_params["maxPriorityFeePerGas"] = 1000000000
 
+            print(f"Tx params after: {tx_params}")
             
             estimated_gas = self.web3.eth.estimate_gas(tx_params)
             print(f"Estimated gas: {estimated_gas}")
